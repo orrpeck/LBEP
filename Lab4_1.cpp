79a1665cf7c5e0043115cb2c9919875846a8155b
Long <longdeptrai1401@gmail.com>
2019-12-09 20:44:35 +0700

    4

--- Lab4_1.cpp
+++ Lab4_1.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(){
-	float n, i=2, S=0;
+	float n, i=1, S=0;
 	printf("Nhập vào n=\n");
 	scanf("%f",&n);
 	while(i<=n){
-		S=1+1/i;
+
+		S=S+1/i;
 		i++;
 	}
 	printf("S = %f\n",S );
