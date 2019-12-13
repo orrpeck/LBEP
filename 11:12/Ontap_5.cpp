d0468fe06936be4cfcd334b16409f554b31abc5b
Long <longdeptrai1401@gmail.com>
2019-12-13 18:05:13 +0700

    Ontap

--- 11:12/Ontap_5.cpp
+++ 11:12/Ontap_5.cpp
@@ -1,17 +1,16 @@
 #include <stdio.h>
 int main(){
-	int n,count=0;
+	int n;
 	printf("Nhập vào số n: ");
 	scanf("%d",&n);
-	for(int i=2; i<=n; i++){
+	for(int i=2; i<n; i++){
+		int count=0;
 		for(int j=1; j<=i; j++){
 			if(i%j==0)
 				count++;
+		}
 			if(count==2)
 				printf("%d, ",i);
-			count=0;
-			//Em làm chưa xong :((
-		}
 	}
 return 0;
 }
--- 11:12/Ontap_6.cpp
+++ 11:12/Ontap_6.cpp
@@ -6,7 +6,7 @@ int main(){
 		scanf("%d",&a);
 		printf("Nhập vào b = ");
 		scanf("%d",&b);
-	}while(a>b);
+	}while(a>=b);
 		for(int i=a; i<=b; i++)
 			tong+=i;
 		printf("Tổng từ %d đến %d bằng %d\n",a,b,tong);
