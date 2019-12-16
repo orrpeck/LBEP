ec5d0cd73a8bc8997746ddbf75a28695904d38c1
Long <longdeptrai1401@gmail.com>
2019-12-16 18:11:35 +0700

    Update Assignment6_4.cpp

--- 13:12/Assignment6_4.cpp
+++ 13:12/Assignment6_4.cpp
@@ -3,13 +3,19 @@ int main(){
 	int n,memory=0;
 	printf("Nhập số lượng số: ");
 	scanf("%d",&n);
-	int ary[n];
+	int ary[n],ar[n];
 	for(int i=0; i<n; i++){
-		do{
-			printf("Nhập vào số thứ %d: ",i+1);
-			scanf("%d",&ary[i]);
-		}while(ary[i]==memory);
-		memory=ary[i];
+		printf("Nhập vào số thứ %d: ",i+1);
+		scanf("%d",&ary[i]);
+		for(int j=0;j<i;j++){
+			if(ary[i]==ary[j]){
+				printf("Nhập lại số thứ %d: ",i+1);
+				scanf("%d",&ary[i]);
+				j=0;
+			}
+		}	
 	}
+	for(int i=0;i<n;i++)
+		printf("Số thứ %d là %d \n",i+1,ary[i]);
 return 0;
 }
